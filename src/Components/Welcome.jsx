29d3e6135ef6eb77a24319bc1a584652873835d3
aafrinabid <mohdaafrin@gmail.com>
2022-10-02 18:55:51 +0530

    added the table

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Welcome({username}) {
+  return (
+    <div>Welcome {username}</div>
+  )
+}
+
+export default Welcome
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import DetailsTable from '../Components/DetailsTable'
+import MessageBox from '../Components/MessageBox'
 import ButtonAppBar from '../Components/ui/NavBar'
+import Welcome from '../Components/Welcome'
 
 function Home() {
+    const role=useSelector(state=>state.authHandler.role)
+    const [details,setDetails]=useState([])
+    console.log(details)
+    const [username,setUsername]=useState('')
+    useEffect(()=>{
+    axios.get('http://localhost:4000/getDetails',{
+        headers:{
+            'x-access-token':localStorage.getItem('token')?localStorage.getItem('token'):""
+        }
+    }).then(res=>{
+        setDetails([...res.data.details])
+        setUsername(res.data.username)
+    })
+
+    },[])
   return (
     <div>
         <ButtonAppBar />
+        <Welcome username={role==1?'admin':username}/>
+ {role===0 &&      <MessageBox />}
+       <DetailsTable userState={role==0?true:false} rows={details}/>
+
     </div>
   )
 }
